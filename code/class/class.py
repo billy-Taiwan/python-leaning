e2157a3dec6cc9e8b86fcac99e0d3bf1768a1087
Billy <111029@stu.cchs.chc.edu.tw>
2023-03-19 14:07:06 +0800

    class的學習

---
+++ code/class/class.py
@@ -0,0 +1,16 @@
+class Phone():
+    def __init__(self,name,size,price,pixel):
+        self.size=size
+        self.price=price
+        self.pixel=pixel
+        self.name=name
+    def show_info(self):
+        print("手機廠牌",self.name)
+        print('手機尺寸',self.size)
+        print('手機價格',self.price)
+        print('手機像素',self.pixel)
+iphone=Phone('蘋果手機',9,45000,12000000)
+iphone.show_info()
+samsung=Phone('三星手機',10,40000,100000000)
+samsung.show_info()
+
